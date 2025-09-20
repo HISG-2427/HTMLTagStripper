b3bb7ac165d8053c7fff27c8901e5cf5c92abcfa
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-20 15:21:49 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const content = document.querySelector(".content");
+
+function buttonClick() {
+    content.innerHTML = `${input.value}`;
+    const blob = new Blob([content], { type: 'text/plain' });
+    const a = document.createElement('a');
+    a.href = URL.createObjectURL(blob);
+    a.download = "result.txt";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(a.href);
+    input.value = "";
+}
